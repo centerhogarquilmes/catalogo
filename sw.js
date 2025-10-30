0dd300f5ff2914a9498bde52679c85737f553533
Center Hogar <chquilmes1260@gmail.com>
2025-10-30 03:54:52 -0300

    ytfhg

--- sw.js
+++ sw.js
@@ -1,28 +1,35 @@
-// sw.js
-const CACHE_NAME = 'calculadora-v1';
+const CACHE_NAME = 'calculadora-v2';
 const urlsToCache = [
-  './',
-  './catalogo/',
-  './calculadora.html',
-  './calculadora.css',
-  './calculadora.js',
-  './manifest.json',
-  './images/logo_192.png',
-  './images/logo_512.png'
+  '/catalogo/',
+  '/catalogo/calculadora.html',
+  '/catalogo/calculadora.css',
+  '/catalogo/calculadora.js',
+  '/catalogo/manifest.json',
+  '/catalogo/images/logo_192.png',
+  '/catalogo/images/logo_512.png'
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => 
+      Promise.all(
+        cacheNames.map((name) => 
+          name !== CACHE_NAME && caches.delete(name)
+        )
+      )
+    )
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
